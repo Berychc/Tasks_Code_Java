402924dde66ea5b8f40e112be8fd93abed765f88
Berychc <treewincsgo@gmail.com>
2025-04-02 22:47:38 +0300

    Add one method to FirstPage

--- src/main/java/org/example/FirstPage.java
+++ src/main/java/org/example/FirstPage.java
@@ -62,4 +62,18 @@ public class FirstPage {
         }
         return result;
     }
+
+    private static int reverse(int x) {
+        long sum = 0;
+
+        while (x != 0) {
+            int rem = x % 10;
+            sum = sum * 10 + rem;
+            x = x / 10;
+        }
+        if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int)sum;
+    }
 }
